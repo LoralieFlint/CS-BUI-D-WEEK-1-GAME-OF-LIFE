4518d94c176cff66e35d09c327e419b628398274
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-10-11 13:46:43 -0700

    react app created and background working

---
+++ game-of-life/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div >
+      <img className="background"
+        src={require('./images/unnamed.gif')}
+        alt="moving game of life background"
+      />
+      </div>
+    </div>
+  );
+}
+
+export default App;
