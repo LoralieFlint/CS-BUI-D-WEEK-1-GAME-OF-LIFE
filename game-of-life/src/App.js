5ee1870e10fd8b410bda1b15096ce288b435f653
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-10-11 17:31:54 -0700

    just need to add about info for the the game of life and maybe the presets then its logic time

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,16 +1,54 @@
 import React from "react";
 import "./App.css";
+import styled from 'styled-components'
+
+const Title = styled.h1`
+color: green;
+font-weight: 900;
+font-family: 'Anton', sans-serif;
+font-size: 45px
+`
+const Rules = styled.h3 `
+color: teal;
+font-family: 'Anton', sans-serif;
+font-size: 25px
+`
+const List = styled.li`
+color: teal;
+font-family: 'Anton', sans-serif;
+font-size: 15px
+`
+const LearnMore = styled.button`
+background-color: black;
+color: teal;
+padding: 7px;
+border-radius: 15px;
+border 1px solid gray;
+margin-top: 10px;
+font weight: 900;
+&:hover {
+  background-color: white;
+  color: black;
+}
+`
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <div >
-      <img className="background"
-        src={require('./images/unnamed.gif')}
-        alt="moving game of life background"
-      />
+      <Title>Conways Game Of Life</Title>
+      <Rules>Rules of the Game:</Rules>
+      <List className="topListItem">
+      If a cell is "alive" and has 2-3 neighbors it remains alive. Else it dies
+      </List>
+      <List>
+      If a cell is "dead" and has exactly 3 neighbors, then it comes to life. Else it dies.
+      </List>
+      <LearnMore>Learn More</LearnMore>
       </div>
-    </div>
+   
   );
 }
 
